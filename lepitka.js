8cb3dd40e373fbf8d0428459d070147e3e98a4b2
Ludvík Prokopec <39367469+ujikCz@users.noreply.github.com>
2020-03-27 12:26:51 +0100

    Create lepitka.js

---
+++ lepitka.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', function(event){
+
+/* všechny objekty který mají atributy v selectoru */
+Array.from(document.querySelectorAll('div.sticker[data-author][data-id]')).forEach(function(elem) {
+  let author = elem.getAttribute('data-author');
+  let id = elem.getAttribute('data-id');
+  let value = elem.innerHTML; /* defaultní hodnota lepitka */
+
+  /* zápis obsahu */
+  elem.innerHTML = '' +
+      '<div class="container">' +
+          '<h4><b>autor: ' + author + '</b></h4>' +
+          '<p>id: ' + id + '</p>' +
+        '</div>' +
+        '<pre class="pad" contenteditable="true">' +
+            value +
+        '</pre>';
+});
+
+}); //DOM load
